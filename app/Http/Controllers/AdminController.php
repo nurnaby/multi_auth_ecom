0ee43b29335b9025cbf8f56f007a9108becdfcef
nurnaby <sohag.biit@gmail.com>
2022-12-14 22:08:12 +0600

    admin templeting

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function AdminDashboard(){
-        return view('admin.admin_dashbord');
+        return view('admin.index');
     } //end method
     
 }
