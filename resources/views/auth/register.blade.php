005f202e284256b49f24b6c58efc8b2de235e4cd
nurnaby <sohag.biit@gmail.com>
2022-12-21 13:17:42 +0600

    register page work

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,114 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('frontend.frontend_master')
+@section('frontend')
+@section('title')
+    Reginter Easy Shope Online Store
+@endsection
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+<div class="page-header breadcrumb-wrap">
+    <div class="container">
+        <div class="breadcrumb">
+            <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
+            <span></span> Pages <span></span> My Account
+        </div>
+    </div>
+</div>
+<div class="page-content pt-150 pb-150">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
+                <div class="row">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="login_wrap widget-taber-content background-white">
+                            <div class="padding_eight_all bg-white">
+                                <div class="heading_s1">
+                                    <h1 class="mb-5">Create an Account</h1>
+                                    <p class="mb-30">Already have an account? <a href="{{ route('login') }}">Login</a>
+                                    </p>
+                                </div>
+                                <form method="POST" action="{{ route('register') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <input type="text" id="name" required="" name="name"
+                                            placeholder="Username" />
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" required="" id="email" name="email"
+                                            placeholder="Email" />
+                                    </div>
+                                    <div class="form-group">
+                                        <input required="" id="password" type="password" name="password"
+                                            placeholder="Password" />
+                                    </div>
+                                    <div class="form-group">
+                                        <input required="" id="password_confirmation" type="password"
+                                            name="password_confirmation" placeholder="Confirm password" />
+                                    </div>
 
-            <!-- Name -->
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                    {{-- <div class="payment_option mb-50">
+                                        <div class="custome-radio">
+                                            <input class="form-check-input" required="" type="radio"
+                                                name="payment_option" id="exampleRadios3" checked="" />
+                                            <label class="form-check-label" for="exampleRadios3"
+                                                data-bs-toggle="collapse" data-target="#bankTranfer"
+                                                aria-controls="bankTranfer">I am a customer</label>
+                                        </div>
+                                        <div class="custome-radio">
+                                            <input class="form-check-input" required="" type="radio"
+                                                name="payment_option" id="exampleRadios4" checked="" />
+                                            <label class="form-check-label" for="exampleRadios4"
+                                                data-bs-toggle="collapse" data-target="#checkPayment"
+                                                aria-controls="checkPayment">I am a vendor</label>
+                                        </div>
+                                    </div> --}}
+                                    <div class="login_footer form-group mb-50">
+                                        <div class="chek-form">
+                                            <div class="custome-checkbox">
+                                                <input class="form-check-input" type="checkbox" name="checkbox"
+                                                    id="exampleCheckbox12" value="" />
+                                                <label class="form-check-label" for="exampleCheckbox12"><span>I agree to
+                                                        terms &amp; Policy.</span></label>
+                                            </div>
+                                        </div>
+                                        <a href="page-privacy-policy.html"><i
+                                                class="fi-rs-book-alt mr-5 text-muted"></i>Lean more</a>
+                                    </div>
+
+                                    <div class="form-group mb-30">
+                                        <button type="submit"
+                                            class="btn btn-fill-out btn-block hover-up font-weight-bold"
+                                            name="login">Submit &amp; Register</button>
+                                    </div>
+                                    <p class="font-xs text-muted"><strong>Note:</strong>Your personal data will be used
+                                        to support your experience throughout this website, to manage access to your
+                                        account, and for other purposes described in our privacy policy</p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 pr-30 d-none d-lg-block">
+                        <div class="card-login mt-115">
+                            <a href="#" class="social-login facebook-login">
+                                <img src="{{ asset('frontend/assets/imgs/theme/icons/logo-facebook.svg') }}"
+                                    alt="" />
+                                <span>Continue with Facebook</span>
+                            </a>
+                            <a href="#" class="social-login google-login">
+                                <img src="{{ asset('frontend/assets/imgs/theme/icons/logo-google.svg') }}"
+                                    alt="" />
+                                <span>Continue with Google</span>
+                            </a>
+                            <a href="#" class="social-login apple-login">
+                                <img src="{{ asset('frontend/assets/imgs/theme/icons/logo-apple.svg') }}"
+                                    alt="" />
+                                <span>Continue with Apple</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
+</div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-            </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-
-                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-primary-button class="ml-4">
-                    {{ __('Register') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@endsection
